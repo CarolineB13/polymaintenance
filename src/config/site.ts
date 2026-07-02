c8f315581edd3e566edcf44f0761be6f571f798f
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2026-07-02 04:44:19 +0200

    🎉 Initialisation du projet Astro

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export const site = {
+    name: "Polymaintenance",
+
+    url: "https://polymaintenance.fr",
+
+    title:
+        "Polymaintenance | Maintenance industrielle en Ardèche",
+
+    description:
+        "Maintenance industrielle, installation de machines, transfert d'ateliers et négoce de matériel professionnel en Ardèche et dans la Drôme.",
+
+    author: "Happyclicks",
+
+    phone: "06 32 94 23 50",
+
+    email: "contact@polymaintenance.fr",
+};
